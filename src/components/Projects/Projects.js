fba2b7e0b25c0892305e480ee606b47804ff954e
ahmad.shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-07-12 23:21:53 +0300

    Add new Project card with live preview link.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import cash from "../../Assets/Projects/cashbox.png";
-import tasky from "../../Assets/Projects/tasky.png";
-import sls from "../../Assets/Projects/SLS.png";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProjectCard from './ProjectCards';
+import Particle from '../Particle';
+import cash from '../../Assets/Projects/cashbox.png';
+import tasky from '../../Assets/Projects/tasky.png';
+import sls from '../../Assets/Projects/SLS.png';
+import AD from '../../Assets/Projects/Admin-Dashboard.png';
 
 function Projects() {
   return (
@@ -14,17 +15,15 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <p style={{ color: 'white' }}>Here are a few projects I've worked on recently.</p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={tasky}
               isBlog={false}
               title="Tasky"
               description="visual project management ability that uses boards, lists, and cards to help teams and individuals organize tasks, collaborate, and track progress."
-              link='https:\\taskyfront.ba-tech.tech'
+              link="https:\\taskyfront.ba-tech.tech"
             />
           </Col>
 
@@ -34,7 +33,6 @@ function Projects() {
               isBlog={false}
               title="SLS Dashboard"
               description="A dashboard to control and customize an Arduino product by connecting the web with Arduino server."
-
             />
           </Col>
 
@@ -44,7 +42,16 @@ function Projects() {
               isBlog={false}
               title="Cashbox Dashboard"
               description="A dashboard to control the company cash system and archive it in tables and cards and create bills to each Sales and purchases"
-              link='https:\\cashbox.ba-tech.tech'
+              link="https:\\cashbox.ba-tech.tech"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={AD}
+              isBlog={true}
+              title="Admin Dashboard"
+              description="Simple dashboard using react, material UI and other third party react libraries to improve react UI skills."
+              link="https://ahmad-shobasi.github.io/Admin-Dashboard"
             />
           </Col>
         </Row>
