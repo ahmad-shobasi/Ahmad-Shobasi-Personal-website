235068e34b5110e22672b1f89ea36719d3ec63ba
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-06-10 15:18:26 +0300

    Create constants.js
    Set global links to share in the root.

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SocialLinks = {
+  gitHub: "https://github.com/ahmad-shobasi",
+  LinkedIn: "https://www.linkedin.com/in/ahmad-shobasi-7a0547243",
+  Facebook: "",
+  Instagram: "https://www.instagram.com/ahmad_shobasi",
+  Twitter: "https://x.com/Ahmed_Shobasi",
+};
